3618713e928636d3fe1cdca7f9585decdf8c4b5c
Christinalulu <christinalu1996@gmail.com>
2022-02-15 14:47:18 +0100

    today_15_feb-CLF

--- 5-Filter-Method-example/js/script.js
+++ 5-Filter-Method-example/js/script.js
@@ -1,3 +1,46 @@
 // Filter method Example
 
 // Example 1 Filter Method with Static Data
+// Filter method Example
+
+// Example 1 Filter Method with Static Data
+
+import teams from"./data/teams.js";
+console.log(teams);
+
+// Seleact the class for team container
+const teamContainer = document.querySelector(".team-container");
+// Select the class for search
+const  search = document.querySelector(".search");
+// creat a variable for the array teams
+let teamsToRender = teams;
+// creat a for loop
+function renderTeams(someArr){
+    // clear the team container
+    teamContainer.innerHTML = "";
+    for(let j = 0; j < someArr.length; j++){
+        console.log(someArr[j]);
+        teamContainer.innerHTML += ` <div class="team"><h3>${someArr[j].full_name}</h3></div>`
+    }
+}
+renderTeams(teamsToRender);
+
+
+search.onkeyup = function (event) {
+    console.log(event);
+    console.log(event.target.value);
+    const searchValue = event.target.value.trim().toLowerCase();
+    console.log(searchValue);
+
+    const filteredTeams = teamsToRender.filter(function (team){
+        if(team.full_name.toLowerCase().startsWith(searchValue)){
+            return true;
+        }
+    })
+    console.log(filteredTeams)
+    // teamsToRender = filteredTeams;
+    console.log("teams to render" , teamsToRender);
+    renderTeams(filteredTeams);
+}
+
+
